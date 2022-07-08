dc831ced56255e5ee49db17f56324d92e52ea00e
olhazolotar <olchere@gmail.com>
2022-07-08 14:56:24 +0100

    add v3

--- src/js/renderCountryListMarkup.js
+++ src/js/renderCountryListMarkup.js
@@ -2,9 +2,9 @@ export function renderCountryListMarkup(countryies) {
     for (const country of countryies) {
         const { name: official, capital, population, flags: svg, languages } = country;
   
-         return countryies.map(country => `<li>
-<img src="${country.flags.svg}" alt="${country.name.official}" width="50">
-<span> ${country.name.official} </span>
+         return countryies.map(country => `<li class='country-list__item'>
+<img class='country-list__img' src="${country.flags.svg}" alt="${country.name.official}" width="50">
+<span class='country-list__name-list'> ${country.name.official} </span>
 </li>`).join('');
     }   
     
--- src/js/renderCountryMurkup.js
+++ src/js/renderCountryMurkup.js
@@ -4,9 +4,9 @@ export function renderCountryMurkup(countryies) {
 
         const langua = Object.values(languages).join(', ');
         
-    return `<li>
-<img src="${country.flags.svg}" alt="${country.name.official}" width="50">
-<span> ${country.name.official} </span>
+    return `<li class='country-list__item'>
+<img class='country-list__img' src="${country.flags.svg}" alt="${country.name.official}" width="100">
+<span class='country-list__name'> ${country.name.official} </span>
 </li>
 <li>Capital: ${capital}  </li>
 <li>Population:  ${population} </li>
